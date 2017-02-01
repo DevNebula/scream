5a5a55ff19e6db6eb0090c576cb910b25a580c9a
kabirkalsi <kabir_7@hotmail.com>
2017-02-01 11:59:43 +0000

    Fix Kernel Modules Not Backing UP/Restore Script :D

--- build/META-INF/com/google/android/aroma/bbackup.sh
+++ build/META-INF/com/google/android/aroma/bbackup.sh
@@ -1,2 +1,2 @@
 #!/sbin/sh
-dd if=/dev/block/bootdevice/by-name/boot of=/sdcard/Scream/Backup/boot.img && zip -r modules.zip /system/lib/modules && mv modules.zip /sdcard/Scream/Backup
+dd if=/dev/block/bootdevice/by-name/boot of=/sdcard/Scream/Backup/boot.img
